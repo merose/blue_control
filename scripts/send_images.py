c80cd10b4aeb723e6d095b31a6614b151bd2022e
Mark Rose <markrose@acm.org>
2023-03-26 18:20:28 +0000

    Add options to crop and scale the captured images

--- scripts/send_images.py
+++ scripts/send_images.py
@@ -31,23 +31,31 @@ class Capture:
 parser = ArgumentParser('Publish images over 0mq')
 parser.add_argument('--no-capture', action='store_true',
                     help='If specified, avoid capturing images')
-parser.add_argument('--width', type=int, default=800,
-                    help='The image width (default: 800)')
-parser.add_argument('--height', type=int, default=600,
-                    help='The image height (default: 600)')
-parser.add_argument('--port', type=int, default=10001,
-                    help='Publisher port')
+parser.add_argument('--frame-rate', type=int, default=5,
+                    help='The frame rate in frames/sec (default: 5)')
+parser.add_argument('--width', type=int, default=1280,
+                    help='The image width (default: 1280)')
+parser.add_argument('--height', type=int, default=720,
+                    help='The image height (default: 720)')
+parser.add_argument('--port', type=int, default=10002,
+                    help='Publisher port (default: 10002)')
 parser.add_argument('--device', type=int, default=0,
                     help='The video device to use (default: 0)')
 parser.add_argument('--quality', type=int, default=95,
                     help='JPEG quality to use (default: 95)')
+parser.add_argument('--crop-left', type=int, default=80,
+                    help='Amount to crop on the left edge (default: 80)')
+parser.add_argument('--crop-right', type=int, default=80,
+                    help='Amount to crop on the right edge (default: 80)')
+parser.add_argument('--scale', type=float, help='Scale factor for result image')
 
 args = parser.parse_args()
 
 if not args.no_capture:
     cap = cv.VideoCapture(args.device)
-    cap.set(cv.CAP_PROP_FRAME_WIDTH, args.width)
     cap.set(cv.CAP_PROP_FRAME_HEIGHT, args.height)
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, args.width)
+    cap.set(cv.CAP_PROP_FPS, args.frame_rate)
 
 capture = Capture(cap)
 Thread(target=capture.run).start()
@@ -60,12 +68,20 @@ print(f'Ready to serve images')
 count = 0
 while True:
     msg = sock.recv_json()
-    quality = msg['quality']
+    quality = msg['quality'] if 'quality' in msg else args.quality
     if args.no_capture:
+        time.sleep(1/args.frame_rate)
         frame = np.zeros((args.height, args.width, 3), np.uint8)
     else:
         frame = capture.get_frame()
     now = time.time()
+    if args.crop_right > 0:
+        frame = frame[:, args.crop_left:-args.crop_right, :]
+    else:
+        frame = frame[:, args.crop_left:, :]
+    if args.scale:
+        frame = cv.resize(frame, None, fx=args.scale, fy=args.scale)
+
     ret, compressed = cv.imencode('.jpg', frame,
                                   [cv.IMWRITE_JPEG_QUALITY, quality])
 
