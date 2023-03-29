ce4d7748709b25a395a86bcda2bda2eb7f71b021
Mark Rose <markrose@acm.org>
2023-03-29 22:53:13 +0000

    Augment JSON test program

--- src/json_test.cpp
+++ src/json_test.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <czmq.h>
@@ -7,12 +8,15 @@ using json = nlohmann::json;
 
 
 int main(int argc, char **argv) {
-    json obj = json::parse("{\"x\": 4, \"y\": \"hello\"}");
+    json obj = json::parse("{\"x\": 4, \"y\": \"hello\", \"servo1\": 123}");
 
     printf("x=%d\n", (int) obj["x"]);
     printf("y=%s\n", ((std::string) obj["y"]).c_str());
     printf("x is null: %d, unknown is null: %d\n",
        obj["x"].is_null(), obj["unknown"].is_null());
+    std::string name = "servo";
+    name += 1;
+    std::cout << name << " is null: " << obj[name].is_null() << std::endl;
 
     return 0;
 }
